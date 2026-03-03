5266895cc597b36daec86e2322ecfefb98cd0984
goDev <nguyentrongtiendev06@gmail.com>
2026-03-03 19:43:25 +0700

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <p className="text-[10em] font-bold text-blue-600">
+        Tailwind ok 🎉
+      </p>
+    </div>
+  )
+}
+
+export default App
